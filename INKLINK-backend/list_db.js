157740c85898dcdc47b8d2304ed30529fa1e4a9a
medmoatazsarray <medmoatazsarray@gmail.com>
2026-04-17 16:42:31 +0100

    a lot

--- INKLINK-backend/list_db.js
+++ INKLINK-backend/list_db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'inklink',
+  database: process.env.DB_NAME || 'sarraymoataz_inklink',
 });
 
 db.connect((err) => {
